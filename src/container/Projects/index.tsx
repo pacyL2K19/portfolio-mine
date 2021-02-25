cf2d254a08cdc3e0d3dd0710e4f2c644db6d611a
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-26 00:01:24 +0100

    Add the title to the main content component

--- src/container/Projects/index.tsx
+++ src/container/Projects/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Text } from '../../components/Home/styled';
+import { theme } from '../../core/theme';
+import projects from '../../helper/projects.json';
+
 interface Props {
 
 }
+
 const Projects: React.FC<Props> = () => {
     return (
         <>
-            
+            <div>
+                <Text color="#ooo" fontSize={theme.fonts?.title.fontSize} fontFamilly={theme.fonts?.title.fontFamilly}>
+                    {projects.title}
+                </Text>
+            </div>  
         </>
     )
 }
--- src/container/Projects/styled.ts
+++ src/container/Projects/styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
-const Box = styled.section`
-    
+export const Box = styled.section`
+    padding: 80px 2%;
 `
