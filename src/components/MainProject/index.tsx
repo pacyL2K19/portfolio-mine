3ef81b1bf9171befaacf966de16bc59fbb569530
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-30 23:50:10 +0200

    Add the main project compoenent

--- src/components/MainProject/index.tsx
+++ src/components/MainProject/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import projects from '../../helper/projects.json';
-import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col, Image } from "react-bootstrap";
 interface Props {
 
 }
@@ -10,17 +10,38 @@ const MainProject: React.FC<Props> = () => {
         <>
             <Container fluid>
                 <Row >
-                    <Col style=
-                        {{
-                            height: 400,
-                            backgroundColor: "#c78c20",
-                            borderRadius: 10
-                        }}
+                    <Col 
+                        style=
+                            {{
+                                height: 450,
+                                backgroundColor: "#c78c20",
+                                borderRadius: 10,
+                                display: "flex",
+                                alignItems: 'center',
+                                justifyContent: "center",
+                                padding: "auto 200px"
+                            }}
                     >
-
+                        <Image
+                            src={projects.projects[0].imgUrl}
+                            fluid
+                        />
                     </Col>
                     <Col>
-                    
+                        <Row>
+                            <Col lg={'12'}>
+                                <p className="h2">{ projects.projects[0].title }</p>
+                            </Col>
+                            <Col lg={'12'}>
+                                <p style={{fontSize: 18}}>{ projects.projects[0].description }</p>
+                            </Col>
+                            <Col lg={'12'}>
+                                
+                            </Col>
+                            <Col lg={'12'}>
+
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
