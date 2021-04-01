985ffe06360d863df535b5cab17a83f722dc2e6f
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-02 00:35:32 +0200

    Prepare te main component

--- src/container/About/index.tsx
+++ src/container/About/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import { Text } from "../../components/Home/styled";
+import { theme } from '../../core/theme';
 interface Props {
 
 }
 const About: React.FC<Props> = () => {
     return (
         <>
-        
+            <Container>
+                <Row>
+                    <Col>
+                        <Text
+                            fontSize={theme.fonts?.title.fontSize}
+                            fontFamilly={theme.fonts?.title.fontFamilly}
+                            color={theme.colors?.mainText}
+                        >
+                            Test
+                        </Text>
+                    </Col>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+                <div style={{}}></div>
+            </Container>
         </>
     )
 }
