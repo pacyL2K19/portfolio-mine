21aace8dc8924121cd40b20738640fe4c9eb1862
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-03 00:05:43 +0200

    Remove unused images

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import { Grid } from '@material-ui/core';
 import { Box, Icon, Profile, Text } from './styled';
 import data from '../../helper/header.json';
 import { theme } from '../../core/theme';
-import profile from '../../assets/profile.jpg';
+import profile from '../../assets/img.png';
 
 const Home : React.FC<Home> = (props: Home) => {
     return (
