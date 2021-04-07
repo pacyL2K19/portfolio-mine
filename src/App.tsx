21f266d189fbb46f3460fc9a5912aed4d1ef9a6e
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-07 23:01:52 +0200

    MInor changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
 
     return (
         <div className = 'main' style={{margin: 0, padding: 0, backgroundColor: theme.colors?.background}}>
-            <Menu navs={["Home", "About", "Contacts"]} />
+            <Menu navs={["Portfolio", "Projects", "About", "Contacts"]} />
             <Header />
             <Project />
             <About />
