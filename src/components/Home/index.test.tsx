0c0f3761af1d30385bf9fc0ce7add94916c4f11f
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-13 18:03:09 +0200

    Minor update

--- src/components/Home/index.test.tsx
+++ src/components/Home/index.test.tsx
@@ -1,13 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import ReactDom from "react-dom";
 import Home from "./index";
+// import 
 
-test("Home contains correct text", () => {
-    render(<Home title="Title test" intro="Intro Test" main="Main test" />);
-    const intro = screen.getByText("Intro Test");
-    const title = screen.getByText("Title test");
-    const main = screen.getByText("Main test");
-    expect(intro).toBeInTheDocument();
-    expect(main).toBeInTheDocument();
-    expect(title).toBeInTheDocument();
-});
