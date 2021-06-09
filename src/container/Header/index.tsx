9c69022e0db42553efb03359a79b4c0d90e94dcc
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-06-09 22:30:13 +0100

    Update the code format on the header compoenent

--- src/container/Header/index.tsx
+++ src/container/Header/index.tsx
@@ -4,11 +4,15 @@ import home from "../../helper/header.json";
 import { theme } from "../../core/theme/index";
 
 const Header: React.FC = () => {
-    return (
-        <div id="portfolio" style={{backgroundColor: theme.colors?.background}}>
-            <Home main = {home.texts.main} title = {home.texts.title} intro = {home.texts.intro} />
-        </div>
-    );
+  return (
+    <div id="portfolio" style={{ backgroundColor: theme.colors?.background }}>
+      <Home
+        main={home.texts.main}
+        title={home.texts.title}
+        intro={home.texts.intro}
+      />
+    </div>
+  );
 };
 
 export default Header;
