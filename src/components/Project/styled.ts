7ecce63636f34d5b41b1a2ea683abe17e3d6d646
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-06-30 14:24:56 +0100

    Refactor the project title to prepare better responsivness

--- src/components/Project/styled.ts
+++ src/components/Project/styled.ts
@@ -2,27 +2,34 @@ import styled from "styled-components";
 import { theme } from "../../core/theme";
 
 export const ProjectBox = styled.div`
-    
-    &:hover {
-        display: flex;
-        justify-content: center;
-        align-items: flex-end;
-    }
+  &:hover {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
 
-    &:hover .overlay {
-        z-index: -1 !important;
-        animation-duration: 3s;
-        animation-name: bgOpacity;
-    }
+  &:hover .overlay {
+    z-index: -1 !important;
+    animation-duration: 3s;
+    animation-name: bgOpacity;
+  }
 
-    &:hover .button {
-        display: block !important;
-        background-color: ${theme.colors?.main};
-        width: 100%;
-        color: ${theme.colors?.mainText};
-        font-weight: bold;
-        text-align: center;
-        font-size: 22px
-    }
+  &:hover .button {
+    display: block !important;
+    background-color: ${theme.colors?.main};
+    width: 100%;
+    color: ${theme.colors?.mainText};
+    font-weight: bold;
+    text-align: center;
+    font-size: 22px;
+  }
+`;
 
+export const ProjectLabel = styled.h3`
+  background-color: ${theme.colors?.main};
+  color: ${theme.colors?.mainText};
+  text-align: center;
+  margin: 40px -20px 0 40px;
+  padding: 10px;
+  border-radius: 0 30px 30px 0;
 `;
